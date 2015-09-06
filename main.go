2719b6226866b53825bec6c2b7b2fa40c8af684e
Adam Patterson <adamthepatterson@gmail.com>
2015-09-05 23:29:50 -0700

    works for fetching city json

--- main.go
+++ main.go
@@ -1,38 +1,68 @@
+/*
+* gunderground
+*
+* Usage:
+*  wunderground dallas,tx
+*
+* Your personal wunderground.com api key must be stored locally in
+* the following file:
+*  wunderground.key
+*
+*/
+
 package main
 
 import (
   "fmt"
-  // "github.com/docopt/docopt-go"
-  // "github.com/jmcvetta/napping"
+  "flag"
+  "os"
+  "strings"
   "net/http"
   "io/ioutil"
 )
 
+var (
+  apiKey string
+)
+
 func main() {
-//   usage := `gunderground.
-//     weather from wunderground.com
-// Usage:
-//   gunderground get seattle,wa
-//   gunderground -v | --version
-//
-// Options:
-//   -h --help     Show this help screen.
-//   -v --version  Show version.`
-//
-//   arguments, _ := docopt.Parse(usage, nil, true, "gunderground 1.0", false)
-//  fmt.Println(arguments)
-  fmt.Println("hi")
-
-  getWeather()
+  if len(os.Args) > 1 {
+    location := parseLocation(os.Args[1])
+    apiKey := readConfig()
+    getWeather(apiKey,location)
+  } else {
+    usage()
+  }
 }
 
-func validateCity() {
-
+func usage() {
+  fmt.Fprintf(os.Stderr, "Usage: %s dallas,tx\n", os.Args[0])
+  flag.PrintDefaults()
+  os.Exit(2)
 }
 
-func getWeather() {
-  city := "http://api.wunderground.com/api/ef11d4f7b98252f9/conditions/q/WA/Seattle.json"
-  response, err := http.Get(city)
+func parseLocation(location string) []string {
+  l := strings.Split(location, ",")
+  return l
+}
+
+func readConfig() string {
+  var s string
+  if conf, err := ioutil.ReadFile("wunderground.key"); err == nil {
+    s := string(conf)
+    return strings.TrimSpace(s)
+  } else {
+    fmt.Println("wunderground.key not found!")
+    os.Exit(0)
+  }
+  return s
+}
+
+func getWeather(key string, location []string) {
+  //city := "http://api.wunderground.com/api/ef11d4f7b98252f9/conditions/q/WA/Seattle.json"
+  urlpath := "http://api.wunderground.com/api/" + key + "/conditions/q/" +
+  location[1] + "/" + location[0] + ".json"
+  response, err := http.Get(urlpath)
   if err != nil {
     fmt.Printf("%s", err)
   } else {
